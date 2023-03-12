56491bb2dcb28910f7525ad33a14fd3dce808ece
Yıldırım Akbal <yildirim.akbal@gmail.com>
2023-03-12 22:12:17 +0300

    Add files via upload
    some unit tests are somehow ....

--- unit_tests.py
+++ unit_tests.py
@@ -1,19 +1,25 @@
 
 from abc import ABC, abstractmethod
- 
-class shape_test(ABC):
-     
+import numpy as np
+
+
+class model_test(ABC):  # This is called abstract base class
     @abstractmethod
-    def test_shapes(self, model, input_):
+    def __init__(self, model):
+        self.model = model
+
+    @abstractmethod
+    def test_shapes(self, model, input):
         pass
-    
+
     @abstractmethod
-    def output_shapes(self):
-        pass
-    
+    def output_shapes(self, x):
+        return x**2
+
     @abstractmethod
-    def count_params(self, model):
-    	pass
-
-
-
+    def count_params(self):
+        lay = 0
+        for i in self.layer.parameters():
+            lay += np.array(i.shape).prod()
+        return lay
+    
