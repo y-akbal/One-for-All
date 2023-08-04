9f0c5533a501d86b9bb35002600a0a3765c4299c
Yıldırım Akbal <yildirim.akbal@gmail.com>
2023-08-04 16:37:22 +0300

    Add files via upload

--- loss_logger.py
+++ loss_logger.py
@@ -4,7 +4,7 @@ import os
 
 class loss_track:
     """
-    Things will work as follows: 
+    Things will work as follows:
 
     Look at loss for an individual batch, update the weights
     update rolling loss, using self.update(loss) update rolling mean loss and
@@ -13,24 +13,28 @@ class loss_track:
     This is important for illustration purposses.
     Pickle file is a dictionary file with the following form:
 
-    {"mean_loss":self._temploss, 
-                 "var_loss":self._tempvar, 
+    {"mean_loss":self._temploss,
+                 "var_loss":self._tempvar,
                  "loss_hist":self._loss_hist,
                  "loss_var_hist":self._lossvar_hist,
                  "num_epochs": self.num_epochs
                  }
-    
+
     As we do not plan to pass a lot of epochs, we may be interested in variance and rolling mean,
-    you can simply write a simple script to pick of mean loss, for each epoch. 
+    you can simply write a simple script to pick of mean loss, for each epoch.
     In the case that you need rolling loss to print, call self.loss. This will return a tuple
-    mean and variance in the epoch. 
+    mean and variance in the epoch.
+
+
+    Turkish: Atanamamış W&B yaptık burada....
     """
 
-    def __init__(self, file_name:str ="loss.log") -> None:
+    def __init__(self, project="time_series_pred", file_name: str = "loss.log") -> None:
         ## File name for logging loss logs ##
         self.file_name = file_name
+        self.project_name = project
         ## --------------- ##
-        ## We do not just keep track the mean loss 
+        ## We do not just keep track the mean loss
         ## but also standard deviation along the batches
         self._temploss = 1e-10
         self._tempvar = 1e-10
@@ -41,26 +45,28 @@ class loss_track:
         self.counter = 1
         self.num_epochs = 0
 
-
         if not os.path.isfile(file_name):
             try:
                 self.pickled_file = file_name
             except Exception as e:
                 print(e)
 
-
-    def update(self, loss:float)-> None:
+    def update(self, loss: float) -> None:
         ## update mean loss and variance loss ##
         difference = (self._temploss - loss) / (self.counter + 1)
-        self._temploss -= difference # to be used to update the variance
-        self._tempvar = ((self.counter-1)/self.counter)*(self._tempvar) + ((loss - self._temploss)**2)/(self.counter)+(difference)**2
+        self._temploss -= difference  # to be used to update the variance
+        self._tempvar = (
+            ((self.counter - 1) / self.counter) * (self._tempvar)
+            + ((loss - self._temploss) ** 2) / (self.counter)
+            + (difference) ** 2
+        )
         ## append the real losses for future analysis
         self._loss_hist.append(self._temploss)
         self._lossvar_hist.append(self._tempvar)
         ## update the counter
         self.counter += 1
 
-    def reset(self)-> None:
+    def reset(self) -> None:
         self.num_epochs += 1
         self.__flush__()
         ### we reset all variables ###
@@ -68,17 +74,18 @@ class loss_track:
         self._tempvar = 1e-10
         self.counter = 1
         ### update the number of epochs passed
-        
 
     def __flush__(self) -> None:
-        dict_ = {"mean_loss":self._temploss, 
-                 "var_loss":self._tempvar, 
-                 "loss_hist":self._loss_hist,
-                 "loss_var_hist":self._lossvar_hist,
-                 "num_epochs": self.num_epochs
-                 }
+        dict_ = {
+            "project_name": self.project_name,
+            "mean_loss": self._temploss,
+            "var_loss": self._tempvar,
+            "loss_hist": self._loss_hist,
+            "loss_var_hist": self._lossvar_hist,
+            "num_epochs": self.num_epochs,
+        }
         num = self.num_epochs
-        with open(f"{num}_epoch"+self.file_name, mode = "ab") as file:
+        with open(f"{num}_epoch" + self.file_name, mode="ab") as file:
             pickle.dump(dict_, file)
 
     @property
@@ -88,6 +95,7 @@ class loss_track:
     @loss.getter
     def loss(self) -> tuple:
         return self._temploss, self._tempvar
+
 
 if __name__ == "__main__":
     pass
