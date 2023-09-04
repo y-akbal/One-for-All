bbc0bdbe21daace17dd1e9c3b81f6acd9258cca6
Yıldırım Akbal <yildirim.akbal@gmail.com>
2023-09-04 07:48:46 +0300

    Add files via upload

--- preprocessor/RF.py
+++ preprocessor/RF.py
@@ -2,6 +2,7 @@ import concurrent.futures
 import math
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
 from preprocess import get_csv_list, preprocess_csv
 import os
 import pandas as pd
@@ -48,35 +49,64 @@ def return_final_result(
     lags: int = 256,
     numerical_column=-1,
     seed=0,
-    regressor=RandomForestRegressor,
+    regressor=LinearRegression,
     **kwargs,
 ):
     preprocessed_ts, _ = preprocess_csv(csv_file, numerical_column=numerical_column)
     numerical_ts = preprocessed_ts.iloc[:, numerical_column].to_numpy()
+
     X_train, y_train, X_test, y_test = return_split(
         numerical_ts, split_ratio=split_ratio, lags=lags
     )
     np.random.seed(seed)
-    rf = regressor(**kwargs, n_jobs=6)
+    rf = regressor(**kwargs)
     rf.fit(X_train, y_train)
     score = rf.score(X_test, y_test)
-    print(f"{csv_file, score} Done")
-    return score, csv_file, score
+    print(f"{csv_file, score, len(numerical_ts)} Done")
+    return score, csv_file
 
 
 def main():
     csv_list = get_csv_list()
-    with concurrent.futures.ProcessPoolExecutor(max_workers=2) as executor:
+    with concurrent.futures.ProcessPoolExecutor(max_workers=10) as executor:
         res = executor.map(return_final_result, csv_list)
     return res
+
+
+def short(len_=2048):
+    list_ = get_csv_list()
+    names = []
+    lengths = []
+    for csv_fil in list_:
+        pd_ser = pd.read_csv(csv_fil, encoding="ISO-8859-1", decimal=",")
+        len__ = len(pd_ser)
+        if len__ < len_:
+            names.append(csv_fil)
+            lengths.append(len__)
+    return names, lengths
+
+
+def return_average(file="pickled.t"):
+    with open(file, "rb") as file_:
+        L = pickle.load(file_)
+    temp_av = 1e-2
+    k = 0
+    list_outlier = []
+    for M in L:
+        value, name = M
+        if 0 < value < 1:
+            temp_av += value
+            k += 1
+        else:
+            list_outlier.append(name)
+    return temp_av / k, list_outlier
 
 
 if __name__ == "__main__":
     a = time.time()
     res = main()
     list_ = list(res)
-    b = time.time()
-    list_ += [b - a]
     with open("pickled.t", mode="wb") as file:
         pickle.dump(list_, file)
-    print(list_)
+    av = return_average()
+    print(av)
