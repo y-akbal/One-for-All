0dec1dd60bb5243ce9bfdd838173f58b760c624f
y-akbal <yildirim.akbal@gmail.com>
2023-09-12 23:22:01 +0300

    sd

--- validate.py
+++ validate.py
@@ -2,7 +2,7 @@ import os
 import torch
 from torch import nn as nn
 from torch.nn import functional as F
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 from main_model import Model
 from memmap_arrays import ts_concatted
 import numpy as np
@@ -11,8 +11,34 @@ import pandas as pd
 import time
 
 
-    
+model = nn.Sequential(*[
+    nn.Linear(100,10),
+    nn.GELU(),
+    nn.Linear(10, 10)
+])    
 
+
+class dd(Dataset):
+    def __init__(self):
+        self.X = np.random.randn(1000, 100)
+        self.y = np.random.randn(1000, 10)
+    def __getitem__(self, i):
+        return self.X[i], self.y[i]
+    def __len__(self):
+        return len(self.X)
+d = dd()
+data = DataLoader(d, 32, drop_last = True, shuffle = False)
+
+
+y_out = np.random.randn(1000, 10)
+y_test = np.random.randn(1000, 10)
+with torch.no_grad():
+    for i, (x,y) in enumerate(data):
+        
+        y_out[i*32:(i+1)*32] = np.array(model(x), dtype = np.float64)
+        y_test[i*32:(i+1)*32] = np.array(y, dtype = np.float64)
+
+torch.randn(10,10).numpy()
 
 def return_dataset(**kwargs):
     memmap_data = np.memmap(kwargs["file"], dtype=np.float32)
@@ -22,33 +48,33 @@ def return_dataset(**kwargs):
     data_ = ts_concatted(**{"array":memmap_data, "lengths": memmap_lengths, "lags": lags})
     return data_
 
+
 def main(**kwargs):
     """
     things to do here
-    1) Create the model
-    2) Adjust the data
+    1) Create the model -- ok
+    2) Load the data -- ok
+    2.5) Save the prediceted and the ground truth
     3) Create a csv file
     4) Compare it with others
     """
     ### First grab the data:
     data = return_dataset(**kwargs)
     batch_size = kwargs["batch_size"]
-    batched_data = DataLoader(data, shuffle = False,**kwargs)
+    batched_data = DataLoader(data, batch_size = batch_size, shuffle = False)
     ## -- ##
     ## Let's load the model from trained file ##    
     file_name = kwargs["file_name"]   
     device =  kwargs["gpu"]
     try:
-        trained_model = Model.from_pretrained(file_name).device()
+        model = Model.from_pretrained(file_name).cuda(device)
     except Exception as exp:
         print(f"Something went wrong with {exp}!!!")
     ### If we come so far everything shoud be good ## Now let's give a try!!!!
     
     
     
-    
-    
-    
+        
 
 if __name__ == "__main__":
     import argparse
