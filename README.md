e2920f0418237a9f55e377025cc2d3dd4e3288d0
y-akbal <yildirim.akbal@gmail.com>
2023-10-26 16:47:43 +0300

    Training loop fixed

--- README.md
+++ README.md
@@ -1,18 +1,12 @@
 # One for All: A Method for forecasting multiple time series in a univariate sense using a single model.
 Some files related to the upcoming manuscript which revolves around ensemble learning for time series forecasting using transformers.
-1) preprocessor.py is responsible for picking up the designated columns and normalizing them. Furthermore, if something goes wrong, a seperate log.txt file will be provided as well as some statistical properties that are obtained as a result of normalization. To do so, you need to adjust the global variables inside the preprocessor.py file. 
-2) layers.py contains some of the layers to be used. For the sake of completeness we implement our own layers,
-3) preprocess.py will contain necessary functions to grab the data out of csv files and do some preprocessing and prepare them,
-4) convert_memmap.py wll convert will grab csv files convert them into data.dat and lengths.dat files.
-5) memmap_arrays.py is a signature snippet, grabbing data.dat and lengths.dat files, and will give you a window of given size from your concatted time series, as X[:-1], X[-1] and place_, where place_ indicates which time series to be used.
-6) RF.py is used to do old-school machine learning analysis -  given that all csv.files are preprocessed - you can choose for instance randomforest or KNN sort of old school stuff. Splitting of time series is done on the fly. Multiprocessing is supported. At the end of the procedure, a file will be provided to see the results.
-7) validate.py is for validation purposses, for instance to see R^2, MSE, MAE, MAPE values on both train and validation set.
+1) layers.py contains some of the layers to be used. For the sake of completeness we implement our own layers,
+2) preprocess_train_test.py will contain necessary functions to grab the data out of csv files and do some preprocessing and prepare them,
+3) convert_memmap.py wll convert will grab csv files convert them into data.dat and lengths.dat files.
+4) memmap_arrays.py is a signature snippet, grabbing data.dat and lengths.dat files, and will give you a window of given size from your concatted time series, as X[:-1], X[-1] and place_, where place_ indicates which time series to be used.
+5) RF.py is used to do old-school machine learning analysis -  given that all csv.files are preprocessed - you can choose for instance randomforest or KNN sort of old school stuff. Splitting of time series is done on the fly. Multiprocessing is supported. At the end of the procedure, a file will be provided to see the results.
+6) validate.py is for validation purposses, for instance to see R^2, MSE, MAE, MAPE values on both train and validation set.
 
-
-Things to do:
-  5) There is a little remaining I believe to do!!!
-  6) train_details.py - probably training such a huge model will take a lot of time, therefore we may need to parallelize training process. This file will contain some functions to achieve this.
-  
   
 
 
--- model_train_ddp.py
+++ model_train_ddp.py
@@ -3,14 +3,145 @@ import torch
 from torch import nn as nn
 from torch.nn import functional as F
 from torch.utils.data import DataLoader
-from memmap_arrays import ts_concatted
-import numpy as np
-import time
-from main_model import Model
-## import hydra now
-import hydra
-from omegaconf import DictConfig, OmegaConf
+from torch.nn.parallel import DistributedDataParallel as DDP
+import torch.nn.functional as F
 
-## This is important in the case that you compile the model!!!!
-torch.set_float32_matmul_precision("high")
+class Trainer:
+    def __init__(
+        self,
+        model: torch.nn.Module,
+        train_data: torch.utils.data.DataLoader,
+        val_data: torch.utils.data.DataLoader,
+        optimizer: torch.optim.Optimizer,
+        scheduler: torch.optim.lr_scheduler,
+        gpu_id: int,
+        save_every: int,
+        val_loss_logger=None,
+        train_loss_logger=None,
+        val_accuracy_logger=None,
+        compile=False,
+        use_wnb = True,
+    ) -> None:
+        self.gpu_id = gpu_id
+        self.model_config = model.config
+        self.model = model.to(gpu_id)
+        self.model = DDP(self.model, device_ids=[gpu_id], find_unused_parameters=True)
+
+        if compile:
+            self.model = torch.compile(self.model)
+        ##
+        self.train_data = train_data
+        self.val_data = val_data
+        ##
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+        ##
+        self.save_every = save_every
+        ##
+        self.val_loss_logger = val_loss_logger
+        self.train_loss_logger = train_loss_logger
+        self.val_accuracy_logger = val_accuracy_logger
+        self.use_wnb = use_wnb
+        ##
+        ##
+        self.autocast = torch.autocast
+        self.scaler = torch.cuda.amp.GradScaler()
+        ## training details ##
+        self.current_epoch = 1
+
+        try:
+            self._load_checkpoint("checkpoint.pt")
+            print("Started from where we stopped last time!!!")
+        except Exception as e:
+            print(f"There is a problem with loading the model weights and the problem is: {e}")
+        
+    def _run_batch(self, source, targets, i):
+        ### All the things like low precision training will happen here!!!
+        self.model.train() ## Model in train mode!!!
+        self.optimizer.zero_grad()
+        with self.autocast(device_type="cuda", dtype=torch.bfloat16):
+            output = self.model(source, task = None)
+            loss = F.mse_loss(output, targets)
+
+        if i % 100 == 0:
+            print(f"loss {loss.item()}, {i} batch, from gpu {self.gpu_id} ")
+        ## Update the the gradients here!!! ## 
+        self.scaler.scale(loss).backward()
+        self.scaler.step(self.optimizer)
+        self.scaler.update()
+        self.scheduler.step()
+        ### We log the loss ### 
+
+ 
+    def _run_epoch(self, epoch, report_in_every = 100):
+        # b_sz = len(next(iter(self.train_data))[0])
+        if epoch % report_in_every == 0:
+            print(f"[GPU{self.gpu_id}] Epoch {epoch}")
+        self.train_data.sampler.set_epoch(epoch)
+
+        for i, (source, targets) in enumerate(self.train_data):
+            source = source.to(self.gpu_id, non_blocking=True)
+            targets = targets.to(self.gpu_id, non_blocking=True)
+            self._run_batch(source, targets, i)
+
+    def train(self, max_epochs: int):
+        for epoch in range(max_epochs):
+            self._run_epoch(epoch)
+            if self.gpu_id == 0 and epoch % self.save_every == 0:
+               self._save_checkpoint(epoch)
+            self.validate()
+
+    def validate(self):
+        if self.gpu_id == 0:
+            print("Validation is started!!!")
+        self.model.eval()
+        with torch.no_grad():  ## block tracking gradients
+            for source, targets, _ in self.val_data:
+                source = source.to(self.gpu_id)
+                targets = targets.to(self.gpu_id)
+                output = self.model(source, task = None)  
+                loss = F.mse_loss(output, targets)
+                self.val_loss_logger.update(loss.item())
+                
+                #print(self.val_accuracy_logger.update(accuracy.item()))
+                
+
+            self.val_loss_logger.all_reduce()
+            
+            if self.gpu_id == 0:
+                print(self.val_loss_logger.get_avg_loss(), self.val_accuracy_logger.accuracy)
+                
+                self.val_loss_logger.reset()
+                self.val_accuracy_logger.reset()    
+    ## Some tools ## 
+    def _load_checkpoint(self, checkpoint_file):
+        model_dict = torch.load(checkpoint_file)
+        ### Now the state dict are obtained below ###
+        model_state_dict = model_dict["model_state_dict"]
+        model_optimizer_state = model_dict["optimizer_state"]
+        ### ---Let's load the model states--- ###
+        #self.model = self.model.from_dict(model_config)
+        self.model.load_state_dict(model_state_dict)
+        self.optimizer.load_state_dict(model_optimizer_state)
+        print("Loaded the new model!!!!")
+ 
+    def _save_checkpoint(self, epoch):
+        ### This are the necessary steps to recover the model from the pickled file!!!
+        model_weights = self.model.state_dict()
+        model_config = self.model_config
+        optimizer_state = self.optimizer.state_dict()
+
+        checkpoint = {"model_state_dict":model_weights,
+                      "model_config":model_config,
+                      "optimizer_state":optimizer_state,
+                      "epoch":epoch
+                    }
+        try:
+            PATH = "checkpoint.pt"
+            torch.save(checkpoint, PATH)        
+            print(f"Epoch {epoch} | Training checkpoint saved at {PATH}")
+        except Exception as exp:
+            print(f"Something went wrong with {exp}, the training will start from begining!!!")
+    
+
 
